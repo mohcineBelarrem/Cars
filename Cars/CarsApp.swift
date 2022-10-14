09b5ea3dd264a4faed51bdfd9bf3557a19716509
mohcine Belarrem <mohcine.belarrem@imedia24.de>
2022-10-14 16:45:46 +0100

    Initial Commit

---
+++ Cars/CarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsApp.swift
+//  Cars
+//
+//  Created by Mohcine Belarrem on 14/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
