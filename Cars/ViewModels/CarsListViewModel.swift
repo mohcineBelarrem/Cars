0f440bf44d85d7e8e647a81fb02d24e9862492ec
mohcine Belarrem <mohcine.belarrem@imedia24.de>
2022-10-15 16:34:12 +0100

    prepares model for filtering cars by models and makes

--- Cars/ViewModels/CarsListViewModel.swift
+++ Cars/ViewModels/CarsListViewModel.swift
@@ -10,9 +10,39 @@ import Combine
 
 
 class CarsListViewModel: ObservableObject {
+    let originalCars : [CarDetailViewModel]
     @Published var cars = [CarDetailViewModel]()
 
+    var firstCarId : String {
+        return cars.first?.id.uuidString ?? ""
+    }
+
     init() {
+        self.originalCars = CarService().getCars().map { CarDetailViewModel(car: $0) }
         self.cars = CarService().getCars().map { CarDetailViewModel(car: $0) }
+    }
+
+    func filterByMake(_ make: String) {
+        guard !make.isEmpty else {
+            cars = originalCars
+            return
+        }
+        cars = cars.filter { $0.make == make }
+    }
+
+    func filterByModel(_ model: String) {
+        guard !model.isEmpty else {
+            cars = originalCars
+            return
+        }
+        cars = cars.filter { $0.model == model }
+    }
+
+    var allMakes: [String] {
+        return originalCars.map { $0.make }
+    }
+
+    var allModels: [String] {
+        return originalCars.map { $0.model }
     }
 }
